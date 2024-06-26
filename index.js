7d463667cab52c81e1bee703013d1c548671b1ca
Wangui <elswangui@gmail.com>
2024-06-26 10:14:14 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+console.log(customerName);
+
+function setBestCustomer(){
+    window.bestCustomer =`not bob`
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = `maybe bob`
+}
+const LeastFavoriteCustomer = `a customer`
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = `another customer`
+}
